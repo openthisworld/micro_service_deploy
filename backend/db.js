f8ee6cd474368c7d26e1dffc3dfbe5e99140cbbb
Vlad Huivyk <puchok80@gmail.com>
2023-07-21 15:00:58 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://openthisworld:p@ssw0rd@localhost:5432/main',
+  connectionString: process.env.DATABASE_URL || 'postgresql://openthisworld:p@ssw0rd@postgres:5432/main',
   ssl: {
     rejectUnauthorized: false
   }
