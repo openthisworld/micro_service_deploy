2de4d29e225a7ca8fd95421d4439dac89a2c4162
Vlad Huivyk <puchok80@gmail.com>
2023-07-24 09:42:14 +0300

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,85 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import { fetchTasks, createTask } from './api'; // Обновленный импорт
 
-function App() {
+const App = () => {
   const [tasks, setTasks] = useState([]);
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-  const [dueDate, setDueDate] = useState('');
+  const [newTaskText, setNewTaskText] = useState('');
 
   useEffect(() => {
-    fetchTasks();
+    const fetchData = async () => {
+      try {
+        const tasks = await fetchTasks();
+        setTasks(tasks);
+      } catch (error) {
+        console.error('Error fetching tasks:', error);
+      }
+    };
+
+    fetchData();
   }, []);
 
-  const fetchTasks = async () => {
+  const handleAddTask = async () => {
     try {
-      const response = await axios.get('/api/tasks');
-      setTasks(response.data);
+      const newTask = { text: newTaskText, completed: false };
+      const createdTask = await createTask(newTask);
+      setTasks([...tasks, createdTask]);
+      setNewTaskText('');
     } catch (error) {
-      console.error(error);
+      console.error('Error creating task:', error);
     }
   };
 
-  const createTask = async () => {
-    try {
-      await axios.post('/api/tasks', { title, description, due_date: dueDate });
-      fetchTasks();
-      setTitle('');
-      setDescription('');
-      setDueDate('');
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const updateTask = async (id) => {
-    try {
-      const taskToUpdate = tasks.find((task) => task.id === id);
-      if (!taskToUpdate) return;
-
-      await axios.put(`/api/tasks/${id}`, {
-        title: taskToUpdate.title,
-        description: taskToUpdate.description,
-        due_date: taskToUpdate.due_date,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const deleteTask = async (id) => {
-    try {
-      await axios.delete(`/api/tasks/${id}`);
-      fetchTasks();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  return (
-    <div>
-      <h1>ToDo Manager</h1>
-      <div>
-        <h3>Create New Task</h3>
-        <input type="text" placeholder="Title" value={title} onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" placeholder="Description" value={description} onChange={(e) => setDescription(e.target.value)} />
-        <input type="date" value={dueDate} onChange={(e) => setDueDate(e.target.value)} />
-        <button onClick={createTask}>Create</button>
-      </div>
-      <div>
-        <h3>Task List</h3>
-        <ul>
-          {tasks.map((task) => (
-            <li key={task.id}>
-              {task.title} - {task.description} (Due: {new Date(task.due_date).toLocaleDateString()})
-              <button onClick={() => updateTask(task.id)}>Done</button>
-              <button onClick={() => deleteTask(task.id)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-  );
-}
+  // Остальной код компонента
+};
 
 export default App;
